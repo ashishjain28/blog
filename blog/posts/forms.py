a3bbf6ab75ca0424ea45df32fd309f06749fdbb4
ashishjain28 <ashishjain.2894@gmail.com>
2017-07-05 09:59:28 +0530

    create_post template, view using generic FormView, DetailView url updation

---
+++ blog/posts/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from .models import Post
+
+
+class CreatePostForm(forms.ModelForm):
+
+    def save(self):
+        form = super(CreatePostForm, self).save(self, self.kwargs)
+        form.save()
+
+
+    class Meta:
+        model = Post
+        fields = [
+            "title",
+            "content"
+        ]
